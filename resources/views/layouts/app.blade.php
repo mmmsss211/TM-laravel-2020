2c35df2af5eea6c1d08d90a75bebf4829403da5d
Percival Ian Muico <mmmsss211@gmail.com>
2020-11-18 23:31:29 +0800

    Header work

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,6 +11,19 @@
 </head>
 
 <body class="bg-gray-100">
+    <nav class="p-6 bg-white flex justify-between">
+        <ul class="flex items-center">
+            <li>
+                <a href="*" class="p-3">Home</a>
+            </li>
+            <li>
+                <a href="*" class="p-3">Dashboard</a>
+            </li>
+            <li>
+                <a href="*" class="p-3">Post</a>
+            </li>
+        </ul>
+    </nav>
     @yield('content')
 </body>
 
