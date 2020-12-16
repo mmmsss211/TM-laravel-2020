8b5eed45aaef3eb37a02776f27011eb3cadceb4e
Percival Ian Muico <mmmsss211@gmail.com>
2020-12-16 22:01:01 +0800

    Minor update

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -10,7 +10,8 @@ class PostController extends Controller
 {
     public function index()
     {
-        $posts = Post::get();
+        $posts = Post::paginate(20);
+
         return view('posts.index', [
             'posts' => $posts,
         ]);
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -23,10 +23,12 @@
             @if($posts->count())
                 @foreach($posts as $post)
                     <div class="mb-4">
-                        <a href="" class="font-bold">{{ $post->user->name }}</a><span class="text-gray-600 text-sm">{{ $post->created_at->diffForHumans() }}</span>
+                        <a href="" class="font-bold">{{ $post->user->name }} </a><span class="text-gray-600 text-sm">{{ $post->created_at->diffForHumans() }}</span>
                         <p class="mb-2">{{ $post->body }}</p>
                     </div>
                 @endforeach
+
+                {{ $posts->links() }}
             @else
                 <p>There are no posts</p>
             @endif
